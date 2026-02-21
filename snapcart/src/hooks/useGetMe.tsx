545cc31ff9b79fe9ca20e71f7a8db504685e8517
sourav842741 <souravkumar85055@gmail.com>
2026-02-21 14:27:18 +0530

    refactor(hooks): improve user fetching logic and session handling in useGetMe

--- snapcart/src/hooks/useGetMe.tsx
+++ snapcart/src/hooks/useGetMe.tsx
@@ -3,31 +3,47 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 
 import { AppDispatch, RootState } from "@/redux/store";
 import { setUserData } from "@/redux/userSlice";
 
-
-
 export const useGetMe = () => {
+
   const dispatch = useDispatch<AppDispatch>();
-  const {userData} = useSelector(
+
+  const { userData } = useSelector(
     (state: RootState) => state.user
   );
 
+  const { status } = useSession(); // ⭐ important
+
   useEffect(() => {
+
     const fetchUser = async () => {
+
       try {
-        const res = await axios.get("/api/user/me"); // 🔹 Replace with your API endpoint
+
+        const res = await axios.get("/api/user/me");
+
         dispatch(setUserData(res.data));
-        console.log(res.data)
-      } catch (error) {
-        console.log(error)
+
+        console.log("User:", res.data);
+
+      } catch (error:any) {
+
+        console.log("GetMe Error:", error.response?.data || error.message);
+
       }
+
     };
 
-    if (!userData ) fetchUser();
-  }, [dispatch, userData]);
+    // ⭐ session ready hone ke baad hi call karo
+    if (status === "authenticated" && !userData) {
+      fetchUser();
+    }
+
+  }, [dispatch, userData, status]);
 
   return userData;
 };
