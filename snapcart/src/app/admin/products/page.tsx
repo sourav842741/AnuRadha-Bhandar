5092032a1d9a8b3ce7c24ea4006f3387317118e2
sourav842741 <souravkumar85055@gmail.com>
2026-03-02 19:39:48 +0530

    changes6

--- snapcart/src/app/admin/products/page.tsx
+++ snapcart/src/app/admin/products/page.tsx
@@ -2,14 +2,17 @@ import AdminProductsClient from "@/components/AdminProductsClient";
 import connectDb from "@/lib/db";
 import Grocery from "@/models/grocery.model";
 
+// ⭐ VERY IMPORTANT
+export const dynamic = "force-dynamic";
 
 export default async function AdminProductsPage() {
+
   await connectDb();
 
   const products = await Grocery.find({}).sort({ createdAt: -1 });
 
-  // Convert Mongoose docs to plain objects
   const plainProducts = JSON.parse(JSON.stringify(products));
 
   return <AdminProductsClient products={plainProducts} />;
+
 }
