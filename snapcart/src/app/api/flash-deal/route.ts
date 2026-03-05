c8601e69a92d4b5912067c544e7ba6a7b5f755d7
sourav842741 <souravkumar85055@gmail.com>
2026-03-05 18:42:14 +0530

    srver side 2

--- snapcart/src/app/api/flash-deal/route.ts
+++ snapcart/src/app/api/flash-deal/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connectDb from "@/lib/db";
 import FlashDeal from "@/models/flashDeal.model";
+import "@/models/grocery.model"; // model register
 
 export async function GET() {
   try {
@@ -16,12 +17,13 @@ export async function GET() {
       .sort({ createdAt: -1 });
 
     return NextResponse.json(activeDeals);
+
   } catch (error) {
     console.error("GET ACTIVE FLASH DEALS ERROR:", error);
+
     return NextResponse.json(
       { message: "Failed to fetch flash deals" },
       { status: 500 }
     );
   }
 }
-
