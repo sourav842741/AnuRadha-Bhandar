6bb6ac4da3fb543de6a3bcf06d140b575925725c
sourav842741 <souravkumar85055@gmail.com>
2026-08-16 18:59:04 +0530

    Add environment configuration and update server port handling

--- socketServer/index.js
+++ socketServer/index.js
@@ -127,6 +127,8 @@ app.post("/notify", (req, res) => {
 });
 
 // ----------------------------------------------------
-server.listen(4000, () =>
-  console.log("🔥 Socket Server Running on http://localhost:4000")
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () =>
+  console.log(`🔥 Socket Server Running on http://localhost:${PORT}`)
 );
